be9b0f2fde2752d28a8488bc78d87cb76c116f23
DeiseFreire <deisefreire2103@gmail.com>
2022-05-17 20:48:58 -0300

    Update views.py
    Realizando um redirect para a view index e de ganhamos a não duplicação de código

--- perfis/views.py
+++ perfis/views.py
@@ -1,12 +1,16 @@
 # experiencein/perfis/views.py
 from django.shortcuts import render
+from django.http import HttpResponse
 from perfis.models import Perfil
+# importando redirect
+from django.shortcuts import redirect
 # código comentado
 # modificando a função convidar
 def convidar(request, perfil_id):
     perfil_a_convidar = Perfil.objects.get(id=perfil_id)
     perfil_logado = get_perfil_logado(request)
     perfil_logado.convidar(perfil_a_convidar)
-    return render(request, 'index.html', {'perfis' : Perfil.objects.all()})
+    # realizando redirecionamento
+    return redirect('index')
 def get_perfil_logado(request):
     return Perfil.objects.get(id=1)
