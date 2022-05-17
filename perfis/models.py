3b5616f748078e0ae3d8429b5dfe5b654a31977a
DeiseFreire <deisefreire2103@gmail.com>
2022-05-17 20:42:00 -0300

    Update models.py
    Mais um parâmetro para models.ForeignKey, o related_name

--- perfis/models.py
+++ perfis/models.py
@@ -1,5 +1,5 @@
 # experiencein/perfis/models.py
 # logo abaixo da declaração da classe Perfil
 class Convite(models.Model):
-    solicitante = models.ForeignKey(Perfil, on_delete=models.CASCADE)
-    convidado = models.ForeignKey(Perfil, on_delete=models.CASCADE)
+   solicitante = models.ForeignKey(Perfil, on_delete=models.CASCADE, related_name='convites_feitos')
+   convidado = models.ForeignKey(Perfil, on_delete=models.CASCADE, related_name='convites_recebidos')
