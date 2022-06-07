df6550ca2251dcfbca849977ae7078760d26ee1f
Deise <deisefreire2103@gmail.com>
2022-06-07 14:57:21 -0300

    Validando os dados do formulário

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -13,3 +13,7 @@ class RegistrarUsuarioForm(forms.Form):
            self.adiciona_erro('Por favor, verifique os dados informados')
            valid = False
         return valid
+
+    def adiciona_erro(self, message):
+        errors = self._errors.setdefault(forms.forms.NON_FIELD_ERRORS, forms.utils.ErrorList())
+        errors.append(message)
