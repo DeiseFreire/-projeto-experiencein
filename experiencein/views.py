2ac8bebd4e7c4adb00bb04dbeab1170747dea588
Deise <deisefreire2103@gmail.com>
2022-06-07 12:28:13 -0300

    modulo 10

--- experiencein/views.py
+++
@@ -1,7 +0,0 @@
-# editando o arquivo experiencein/perfis/views.py
-
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse('Bem-vindo ao Experiencein')
